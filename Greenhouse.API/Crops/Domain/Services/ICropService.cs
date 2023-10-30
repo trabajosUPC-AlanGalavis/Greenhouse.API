e3cbc42489b23ab52a2e6517354aa0b209437d8c
Alan Galavis <102612482+trabajosUPC-AlanGalavis@users.noreply.github.com>
2023-10-30 11:47:33 -0500

    feat: Added Crop repository and Service interfaces

---
+++ Greenhouse.API/Crops/Domain/Services/ICropService.cs
@@ -0,0 +1,13 @@
+using Greenhouse.API.Crops.Domain.Models;
+using Greenhouse.API.Crops.Domain.Services.Communication;
+
+namespace Greenhouse.API.Crops.Domain.Services;
+
+public interface ICropService
+{
+    Task<IEnumerable<Crop>> ListAsync();
+    Task<IEnumerable<Crop>> ListByPhaseIdAsync(int phaseId);
+    Task<CropResponse> SaveAsync(Crop crop);
+    Task<CropResponse> UpdateAsync(int cropId, Crop crop);
+    Task<CropResponse> DeleteAsync(int cropId);
+}
