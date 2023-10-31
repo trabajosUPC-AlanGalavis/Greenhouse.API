f7db54755f5d386e0d52c4a69894e589ed95c4a7
Andres (UPC) <u202116113@upc.edu.pe>
2023-10-30 23:34:30 -0500

    feat: added growRoomRecordController

---
+++ Greenhouse.API/Crops/Controllers/GrowRoomRecordsController.cs
@@ -0,0 +1,79 @@
+using AutoMapper;
+using Greenhouse.API.Crops.Domain.Models;
+using Greenhouse.API.Crops.Domain.Services;
+using Greenhouse.API.Crops.Resources;
+using Greenhouse.API.Shared.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greenhouse.API.Crops.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class GrowRoomRecordsController : ControllerBase
+{
+    private readonly IGrowRoomRecordService _growRoomRecordService;
+    private readonly IMapper _mapper;
+    
+    public GrowRoomRecordsController(IGrowRoomRecordService growRoomRecordService, IMapper mapper)
+    {
+        _growRoomRecordService = growRoomRecordService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<GrowRoomRecordResource>> GetAllAsync()
+    {
+        var growRoomRecords = await _growRoomRecordService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<GrowRoomRecord>, IEnumerable<GrowRoomRecordResource>>(growRoomRecords);
+        return resources;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveGrowRoomRecordResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+    
+        var growRoomRecord = _mapper.Map<SaveGrowRoomRecordResource, GrowRoomRecord>(resource);
+    
+        var result = await _growRoomRecordService.SaveAsync(growRoomRecord);
+    
+        if (!result.Success)
+            return BadRequest(result.Message);
+    
+        var cropResource = _mapper.Map<GrowRoomRecord, GrowRoomRecordResource>(result.Resource);
+    
+        return Ok(cropResource);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveGrowRoomRecordResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+    
+        var growRoomRecord = _mapper.Map<SaveGrowRoomRecordResource, GrowRoomRecord>(resource);
+    
+        var result = await _growRoomRecordService.UpdateAsync(id, growRoomRecord);
+    
+        if (!result.Success)
+            return BadRequest(result.Message);
+    
+        var growRoomRecordResource = _mapper.Map<GrowRoomRecord, GrowRoomRecordResource>(result.Resource);
+    
+        return Ok(growRoomRecordResource);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _growRoomRecordService.DeleteAsync(id);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var growRoomRecordResource = _mapper.Map<GrowRoomRecord, GrowRoomRecordResource>(result.Resource);
+
+        return Ok(growRoomRecordResource);
+    } 
+}
