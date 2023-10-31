0b9e393c7ee386a6f52d0ab6bee430302da117de
Andres (UPC) <u202116113@upc.edu.pe>
2023-10-30 23:59:10 -0500

    feat: added employee controller

---
+++ Greenhouse.API/Profiles/Controllers/CompanyEmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Greenhouse.API.Profiles.Domain.Models;
+using Greenhouse.API.Profiles.Domain.Services;
+using Greenhouse.API.Profiles.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Greenhouse.API.Profiles.Controllers;
+
+[ApiController]
+[Route("/api/v1/companies/{companyId}/employees")]
+public class CompanyEmployeesController : ControllerBase
+{
+    private readonly IEmployeeService _employeeService;
+    private readonly IMapper _mapper;
+
+    public CompanyEmployeesController(IEmployeeService employeeService, IMapper mapper)
+    {
+        _employeeService = employeeService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<EmployeeResource>> GetAllByCompanyIdAsync(int companyId)
+    {
+        var employees = await _employeeService.ListByCompanyIdAsync(companyId);
+
+        var resources = _mapper.Map<IEnumerable<Employee>, IEnumerable<EmployeeResource>>(employees);
+
+        return resources;
+    }
+}
